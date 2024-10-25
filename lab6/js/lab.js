d352f19e78aa0228edbeae1a73b81c7f669ef945
linguinediNO <yalorenz@ucsc.edu>
2024-10-24 21:12:04 -0700

    Completed Lab 6: Arrays and Objects

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,7 +1,29 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 6: Arrays and Objects
+// Author: Yadira Lorenzo <yalorenz@ucsc.edu>
+// Date: 22 October
 
+// Define Variblees
+myTransport = ["Ford Mustang Base", "public bus", "my legs"];
+
+
+// create an object for my main ride
+myMainRide = {
+    make: "Ford",
+    model: "Mustang Base",
+    color: "Black",
+    year: 1967,
+    // we can define a function within our object (called a method)
+    // that uses the vaule of year above (using this.year)
+    age: function () {
+      return 2022 - this.year;
+  }
+}
+
+// output
+document.writeln("Kinds of transportion I use: ", myTransport, "</br>");
+// this little trick allows us to write an object to the document
+document.writeln("My Main Ride: <pre>",
+  JSON.stringify(myMainRide, null, '\t'),"</pre>");
 // Constants
 
 // Functions

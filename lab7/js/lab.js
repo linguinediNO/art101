034a96581de1e96e87a3d8b004b79a6f1d9f02a1
linguinediNO <yalorenz@ucsc.edu>
2024-10-28 13:02:34 -0700

    lab 7 completed

---
+++ lab7/js/lab.js
@@ -0,0 +1,45 @@
+// index.js - Lab 7: Functions
+// Author: Yadira Lorenzo <yalorenz@ucsc.edu>
+// Date: 22 October
+
+
+// Constants
+
+// Functions
+
+// sortUserName - a function that takes user input and sorts the letters
+// of their name
+function sortUserName() {
+    var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
+    console.log("userName =", userName);
+    // split string to array
+    var nameArray = userNAme.split('');
+    console.log("nameArray =", nameArray);
+    // sort the array
+    var nameArraySort = nameArray.sort();
+    console.log("nameArraySort =", nameArraySort);
+    //join array back to a string
+    var nameSorted = nameArraySort.join('');
+    console.log("nameSorted =", nameSorted);
+    //Note that I could have done the above line as a signle line:
+    //  userName.toLower().split("").sort().join("")
+    return nameSorted;
+}
+
+// output
+document.writeln("Oh hey, I've fixed your name: ",
+    sortUserName(), "</br>");
+
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+function myFunction(param1, param2) {
+  // some code here
+  // return results;
+}
+
+function main() {
+  console.log("Main function started.");
+  // the code that makes everything happen
+}
+
+// let's get this party started
+main();
